de4805857cffc2ea9f8550c09e4b489b263b4434
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-07-31 19:43:01 +0200

    Alternate sectionn BG Color

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
 		if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
 			$('.chart').easyPieChart({
 				easing: 'easeInOut',
-				barColor: '#fff',
+				barColor: '#F7DF1E',
 				trackColor: false,
 				scaleColor: false,
 				lineWidth: 4,
