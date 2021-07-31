5c191bfb655bb4a312bf6eb1da84c6552398345e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-07-31 21:09:26 +0200

    Laout Settled

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
 	})
 
 	var typed = new Typed('.typed', {
-		strings: ['Software Engineer.', 'Web Developer.', 'Student.'],
+		strings: ['Designer.', 'Web Developer.', 'Student.'],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
